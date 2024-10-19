f91eea85a09a16c39fce4a5b5660fd39edffb98a
Alex <119021072+Zhangzhefu@users.noreply.github.com>
2024-10-19 10:37:08 -0600

    Update Collatz-transform.py

--- Collatz-transform.py
+++ Collatz-transform.py
@@ -1,10 +1,17 @@
-#
-#
-#
-counter = 24
+# This is a transformation of the original Collatz-conjecture question
+# This solves math questions like: 
+# If Bob writes the first n terms using the Collatz-Conjecture sequence and notices that the sum of these terms is a four-digit number. 
+# How many different possible values of n are there?
+
+# This can be modified to use for bigger or smaller numbers 
+
+init_value = int(input("What is the starting value? ")
+
+value = init_value
+counter = init_value
 count_term = 1
 
-while counter < 1000:
+while counter < 1000:    # change this to make it 5 digits, 6 digits... {100, 1000, 10000}
     if value % 2 == 0:
         value = value // 2
     else:
@@ -14,14 +21,13 @@ while counter < 1000:
     count_term += 1
 
 print("the first 4 digit number is " + str(counter))
-#print("the value left off here is " + str(value))
 
 
-sec_value = 24
-sec_counter = 24
+sec_value = init_value
+sec_counter = init_value
 sec_count_term = 1
 
-while sec_counter < 10000:
+while sec_counter < 10000:    # change this to be one digit higher than the variable "counter"
     if sec_value % 2 == 0:
         sec_value = sec_value // 2
     else:
@@ -32,9 +38,5 @@ while sec_counter < 10000:
 
 sec_counter -= sec_value
 print("the last 4 digit number is " + str(sec_counter))
-
-#print("the value left off here is " + str(sec_value))
-#print("the terms it took for this is " + str(count_term))
-#print(sec_counter - sec_value - counter)
 
 print(sec_count_term - count_term)
